93eea91976d5182e862c54a98bc7fac4b4fadd4b
Wes McC <wkmccowin@yahoo.com>
2019-07-16 18:04:24 -0700

    Trying to fix styling issues

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the article information in the articles id.
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].url + "</p>");
+      $("#articles").append("<p data-id='" + data[i]._id + ">" + data[i].headline + "<br />" + data[i].url + "</p>");
     }
   });
 
